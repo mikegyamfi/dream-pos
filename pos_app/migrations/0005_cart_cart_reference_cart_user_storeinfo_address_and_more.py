ea1c0f357bd8cd82264b141bb22f8028c9b6fc6d
mikegyamfi <codeage20@gmail.com>
2023-07-05 10:48:00 +0000

    changes to admin

--- pos_app/migrations/0005_cart_cart_reference_cart_user_storeinfo_address_and_more.py
+++ pos_app/migrations/0005_cart_cart_reference_cart_user_storeinfo_address_and_more.py
@@ -59,17 +59,4 @@ class Migration(migrations.Migration):
             name='cost_price',
             field=models.FloatField(blank=True, default=0.0),
         ),
-        migrations.CreateModel(
-            name='CashierCart',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cart_reference', models.CharField(blank=True, max_length=200, null=True)),
-                ('domain', models.CharField(max_length=250)),
-                ('product_qty', models.PositiveIntegerField()),
-                ('unit_price', models.FloatField()),
-                ('total_price', models.FloatField()),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='pos_app.product')),
-                ('user', models.ForeignKey(default=pos_app.models.get_user, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
     ]
