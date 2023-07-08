f93611ea6330152f6d1da6d4034d698d4c081019
mikegyamfi <codeage20@gmail.com>
2023-07-08 19:48:44 +0000

    changes to ref

--- pos_app/migrations/0007_cashiercart_created_at_alter_individualdaysale_day.py
+++ pos_app/migrations/0007_cashiercart_created_at_alter_individualdaysale_day.py
@@ -12,12 +12,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='cashiercart',
-            name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
-            preserve_default=False,
-        ),
+        # migrations.AddField(
+        #     model_name='cashiercart',
+        #     name='created_at',
+        #     field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+        #     preserve_default=False,
+        # ),
         migrations.AlterField(
             model_name='individualdaysale',
             name='day',
